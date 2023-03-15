1313b5e779985dd3db6336891c7cfde3a4655637
Mohammadreza Jafari <fristhon97@gmail.com>
2023-03-15 20:29:43 +0330

    fix admin url pattern priority

--- pyblog/pyblog/urls.py
+++ pyblog/pyblog/urls.py
@@ -19,9 +19,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),    
     path('',include('base.urls')),
     path('',include('blog.urls')),
     path('users/', include('users.urls')),
-    path('admin/', admin.site.urls),
     *static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 ]
