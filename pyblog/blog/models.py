0abcc318bc6cdafcf48955582a1363521b2e88fc
Mohammadreza Jafari <fristhon97@gmail.com>
2023-03-15 15:57:40 +0330

    add category model

--- pyblog/blog/models.py
+++ pyblog/blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 import textwrap
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name_plural = "categories"
+    
+    def __str__(self) -> str:
+        return self.title
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
