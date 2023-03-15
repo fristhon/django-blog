3b525c5127bf606cb8defe9cfb17d263e1e852b6
Mohammadreza Jafari <fristhon97@gmail.com>
2023-03-15 20:01:25 +0330

    implement basic user authentication

--- pyblog/pyblog/urls.py
+++ pyblog/pyblog/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('',include('base.urls')),
     path('',include('blog.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     *static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 ]
--- pyblog/users/views.py
+++ pyblog/users/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('profile')
+
+    form = UserRegistrationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('login')
+        
+    return render(request, 'users/register.html', {'form':form})
+
+def login(request):
+    if request.user.is_authenticated:
+            return HttpResponseRedirect('profile')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+        if user:
+            form = auth.login(request, user)
+            print(request, f' Welcome {username} !!!!')
+            return HttpResponseRedirect('profile')
+    
+    form = auth.forms.AuthenticationForm()
+    return render(request, 'users/login.html', {'form':form, 'title':'log in'})
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('login')
+    return render(request, 'users/profile.html', {})
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
