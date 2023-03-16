126ffbfa9399576f15ca48acbca4f06bedc43a30
Mohammadreza Jafari <fristhon97@gmail.com>
2023-03-16 22:57:42 +0330

    auto slug for medels

--- pyblog/blog/admin.py
+++ pyblog/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post,Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
