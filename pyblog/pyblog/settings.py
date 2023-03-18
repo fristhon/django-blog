a80a028942bf3f222062a6b741ab4c9e7ff9c916
Mohammadreza Jafari <fristhon97@gmail.com>
2023-03-18 20:04:12 +0330

    read ALLOWED_HOSTS from secret file

--- pyblog/pyblog/settings.py
+++ pyblog/pyblog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secret['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = secret['ALLOWED_HOSTS']
 
 
 # Application definition
